78405f0ee69944be2db6558aab422b88a7716ef3
Ra40kNaMeste <aleksiei.smirnov.2001@gmail.com>
2024-10-01 16:08:33 +0300

    Добавьте файлы проекта.

---
+++ EncaptchaAPI/UserContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.EntityFrameworkCore;
+
+namespace EncaptchaAPI
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
