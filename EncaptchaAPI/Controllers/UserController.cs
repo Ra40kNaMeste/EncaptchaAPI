717a7de585eebb6cdff7e459f04fd4d9e0b4748c
Ra40kNaMeste <aleksiei.smirnov.2001@gmail.com>
2024-10-03 17:23:52 +0300

    Добавил возможность смены роли

--- EncaptchaAPI/Controllers/UserController.cs
+++ EncaptchaAPI/Controllers/UserController.cs
@@ -14,14 +14,14 @@ namespace EncaptchaAPI.Controllers
             _context = context;
         }
 
-        [Route("customers")]
+        [Route("users")]
         [HttpGet]
         public async Task<IAsyncEnumerable<UserView>> GetUsers()
         {
             return _context.Users.Select(i=>i as UserView).AsAsyncEnumerable();
         }
 
-        [Route("customer/{id}")]
+        [Route("user/{id}")]
         [HttpGet]
         public async Task<IActionResult> GetUser(int id)
         {
@@ -38,7 +38,37 @@ namespace EncaptchaAPI.Controllers
             return Ok(new UserView(person));
         }
 
-        [Route("customer/{id}")]
+        [Route("user/{id}")]
+        [Authorize]
+        [HttpPut]
+        public async Task<IActionResult> ChangeRole(int id, Roles role)
+        {
+            var target = await _context.Users.FirstOrDefaultAsync(c => c.Id == id);
+
+            string? email = ControllerContext.HttpContext.User.FindFirst(ClaimTypes.Email)?.Value;
+            var user = await _context.Users.FirstOrDefaultAsync(i => i.Email == email);
+
+            if (user == null)
+                return BadRequest("Authorization error");
+            if (target == null)
+                return BadRequest("User not found");
+
+            //Условие изменения собственной роли обычным смертным
+            var CanSelfCondition = () => user == target && (role == Roles.Employee || role == Roles.Customer);
+
+            //Условие изменения роли для адимнистрации
+            var CanOtherCondition = () => user.Role >= Roles.Admin && user.Role <= target.Role;
+
+            if (!CanSelfCondition() || ! CanOtherCondition())
+                return BadRequest("You don't have enough rights");
+
+            target.Role = role;
+            await _context.SaveChangesAsync();
+            return Ok("Role was change succesfull");
+        }
+
+        [Route("user/{id}")]
+        [Authorize]
         [HttpDelete]
         public async Task<IActionResult> DeleteUser(int id)
         {
